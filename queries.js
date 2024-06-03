7c6888eab1a40caf3538a89e0c37bebdccbba39e
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-06-03 11:01:29 +0500

    Fixed: create table query and added insert query

--- queries.js
+++ queries.js
@@ -1,7 +1,9 @@
+const {db: pool} = require("./config");
 const createTable = () => {
-    pool.query(`CREATE TABLE property_v2 (
+  pool.query(
+    `CREATE TABLE IF NOT EXISTS property_v2 (
       id SERIAL PRIMARY KEY,
-      desc TEXT,
+      "desc" TEXT,
       header TEXT,
       type VARCHAR(255),
       price double precision,
@@ -10,20 +12,52 @@ const createTable = () => {
       area VARCHAR(255),
       purpose VARCHAR(255),
       bedroom VARCHAR(255),
-      added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      added bigint DEFAULT 0,
       initial_amount VARCHAR(255) NULL,
       monthly_installment VARCHAR(255) NULL,
       remaining_installments VARCHAR(255) NULL,
       url TEXT,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  );`, (err, res) => {
-        if (err) {
-          console.error('error creating table:', err);
-          return;
-        }
-        console.log('table created');
-      });      
-}
+  );`,
+    (err, res) => {
+      if (err) {
+        console.error("error creating table:", err);
+        return;
+      }
+      console.log("table created");
+    }
+  );
+};
 
-module.exports = { createTable }
+const insertIntoPropertyV2 = (data) => {
+  pool.query(
+    `INSERT INTO property_v2 ("desc", header, type, price, location, bath, area, purpose, bedroom, added, initial_amount, monthly_installment, remaining_installments, url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)`,
+    [
+      data.desc,
+      data.header,
+      data.type,
+      data.price,
+      data.location,
+      data.bath,
+      data.area,
+      data.purpose,
+      data.bedroom,
+      data.added,
+      data.initial_amount,
+      data.monthly_installment,
+      data.remaining_installments,
+      data.url,
+    ],
+    (err, res) => {
+      if(err) {
+        console.error("error inserting into table:", err);
+        return;
+      } else {
+        console.log("inserted into table");
+      }
+    }
+  );
+};
+
+module.exports = { createTable, insertIntoPropertyV2 };
