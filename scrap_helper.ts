ef47bc3bbfcaad6d0827a2d08f005873ea8d40e1
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-06-05 16:48:27 +0500

    Added puppeteer and scrap stories data

--- scrap_helper.ts
+++ scrap_helper.ts
@@ -1,5 +1,6 @@
 import * as cheerio from "cheerio";
 import axios from "axios";
+import { Browser, Page } from "puppeteer";
 import { insertIntoPropertyV2 } from "./queries";
 import { formatPrice, relativeTimeToTimestamp } from "./utils";
 import { logger as mainLogger } from "./config";
@@ -36,6 +37,54 @@ export const scrapeHtmlPage = async (url: string) => {
   });
 
   return keyValue;
+};
+
+export const scrapStoriesListings = async (url: string, browser: Browser) => {
+  let nextLink: string | null = url;
+  let page: Page | null = null;
+  const storiesUrls: string[] = [];
+  try {
+    page = await browser.newPage();
+    page.on("response", async (response) => {
+      if (response.url().includes("queries")) {
+        const responseData = await response.json();
+        const { results } = responseData;
+        results?.forEach((result: any) => {
+          if (result?.index === "zameen-production-stories-en") {
+            const { hits } = result;
+            if (hits?.length > 0) {
+              hits?.forEach((hit: any) => {
+                storiesUrls.push(
+                  `${process.env.BASE_URL}/property/${hit.slug}.html`
+                );
+              });
+            }
+          }
+        });
+      }
+    });
+    await page.goto(nextLink ?? "");
+    await page.waitForNetworkIdle();
+  } catch (err) {
+    logger.error(err);
+  } finally {
+    if (page) {
+      await page.close();
+    }
+  }
+  const promises = storiesUrls.map((url) => scrapeHtmlPage(url));
+  const data = await Promise.allSettled(promises);
+  await Promise.allSettled(
+    data.map((result) => {
+      if (result.status === "fulfilled") {
+        return insertIntoPropertyV2(result.value);
+      } else {
+        if (result.status === "rejected")
+          logger.error(`Error scraping ${nextLink}: ${result.reason}`);
+        return null;
+      }
+    })
+  );
 };
 
 export const scrapListing = async (url: string, lastAdded: number) => {
