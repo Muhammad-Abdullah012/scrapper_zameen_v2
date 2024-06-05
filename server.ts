9112f19076d0e1fb1cd1c1f1d71e48b76aae3f68
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-06-05 18:23:30 +0500

    Added api to scrap and ping

---
+++ server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { spawn } from "node:child_process";
+
+(() => {
+  spawn("pnpm", ["run", "start-cronjob"], {
+    cwd: process.cwd(),
+    stdio: "inherit",
+  });
+})();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Server is running!");
+});
+
+app.post("/scrap", (req, res) => {
+  try {
+    spawn("pnpm", ["run", "start"], {
+      cwd: process.cwd(),
+      stdio: "inherit",
+    });
+    res.json("Scraping started!");
+  } catch (err) {
+    if (err instanceof Error) {
+      return res.json({ error: err.message });
+    }
+    res.json(JSON.stringify(err));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
